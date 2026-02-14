19e60df02a4b755a245497417fe783f783b1eac6
kei <morisue.kei@gmail.com>
2026-02-14 18:56:12 +0900

    control details

--- src/axanael/control.js
+++ src/axanael/control.js
@@ -57,17 +57,57 @@ export const CTRL = {
         return rev;
     },
 
+    set_scale: (symbol) => {
+        const range_s = document.createElement("input");
+        range_s.type = "range";
+        range_s.min = 0;
+        range_s.max = 5;
+        range_s.step = 0.0001;
+        range_s.value = 1;
+
+        range_s.oninput = (e) => {
+            symbol.params.scale = parseInt(e.target.value);
+            PAINT.redraw();
+        };
+        return range_s;
+    },
+
+    set_offset: (symbol) => {
+        const range_off = document.createElement("input");
+        range_off.type = "range";
+        range_off.min = -5;
+        range_off.max = 5;
+        range_off.step = 0.0001;
+        range_off.value = 1;
+
+        range_off.oninput = (e) => {
+            symbol.params.offset = parseInt(e.target.value);
+            PAINT.redraw();
+        };
+        return range_off;
+    },
+
     set: (body, i, symbol, l) => {
-        const range_d = CTRL.set_range(symbol, l);
-        const rem = CTRL.set_remove(i, l);
-        const flip = CTRL.set_flip(symbol);
-        const rev = CTRL.set_reverse(symbol);
 
         const span = document.createElement("span");
         body.appendChild(span);
-        span.appendChild(flip);
-        span.appendChild(rev);
+
+        if ([0, 1, 2, 3, 6, 7].indexOf(symbol.type) >= 0) {
+            const flip = CTRL.set_flip(symbol);
+            span.appendChild(flip);
+        }
+
+        if ([0, 1, 2, 3, 4, 5, 6, 7].indexOf(symbol.type) >= 0) {
+            const rev = CTRL.set_reverse(symbol);
+            span.appendChild(rev);
+        }
+
+
+        const range_d = CTRL.set_range(symbol, l);
         span.appendChild(range_d);
+
+
+        const rem = CTRL.set_remove(i, l);
         span.appendChild(rem);
 
     },
